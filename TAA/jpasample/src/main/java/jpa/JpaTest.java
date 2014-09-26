bcd5764d5a83b0a14580d953f53e0b001d9407bb
Amona Souliman <ammona.souliman1@gmail.com>
2014-09-26 12:10:21 +0200

    Change: Test persist Fix: GeneratedValue for Passenger Id

--- TAA/jpasample/src/main/java/jpa/JpaTest.java
+++ TAA/jpasample/src/main/java/jpa/JpaTest.java
@@ -1,9 +1,17 @@
 package jpa;
 
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.Collection;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+
+import jpa.domain.Driver;
+import jpa.domain.Passenger;
+import jpa.domain.Ride;
 
 public class JpaTest {
 
@@ -28,8 +36,28 @@ public class JpaTest {
 		try {
 			
 			//manager.persist(et);
+			Driver driver = new Driver();
 			
-
+			Collection<Ride> rides = new ArrayList<Ride>();
+			Ride r = new Ride();
+			r.setDestination("Tahiti");
+			r.setOrigin("Rennes");
+			r.setDriver(driver);
+			r.setSeatNumber(2);
+			r.setLeavingDate(new Date(new java.util.Date().getTime()));
+			
+			Collection<Passenger> ps = new ArrayList<Passenger>();
+			Passenger p = new Passenger();
+			p.setRides(rides);
+			ps.add(p);
+			
+			r.setPassengers(ps);
+			rides.add(r);
+			driver.setRides(rides);
+			
+			manager.persist(driver);
+			manager.persist(r);
+			manager.persist(p);
 					
 		} catch (Exception e) {
 			e.printStackTrace();
--- TAA/jpasample/src/main/java/jpa/domain/Passenger.java
+++ TAA/jpasample/src/main/java/jpa/domain/Passenger.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.Collection;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
@@ -30,6 +31,7 @@ public class Passenger implements Serializable {
 	 * @return id
 	 */
 	@Id
+	@GeneratedValue
 	public int getId() {
 		return id;
 	}
