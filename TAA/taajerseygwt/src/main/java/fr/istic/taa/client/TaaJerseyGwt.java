f0d4fa7d63edd9d78b14f797856bf59aceb61a03
Amona Souliman <ammona.souliman1@gmail.com>
2014-10-16 12:02:38 +0200

    Add: gitignore for data folder & CellTable

--- TAA/taajerseygwt/src/main/java/fr/istic/taa/client/TaaJerseyGwt.java
+++ TAA/taajerseygwt/src/main/java/fr/istic/taa/client/TaaJerseyGwt.java
@@ -9,11 +9,17 @@ import com.google.gwt.http.client.RequestBuilder;
 import com.google.gwt.http.client.RequestCallback;
 import com.google.gwt.http.client.RequestException;
 import com.google.gwt.http.client.Response;
+import com.google.gwt.user.cellview.client.CellTable;
+import com.google.gwt.user.cellview.client.TextColumn;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.view.client.ListDataProvider;
 import fr.istic.taa.shared.IRide;
+import fr.istic.taa.shared.Ride;
+
+import javax.swing.text.TableView;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -27,14 +33,73 @@ public class TaaJerseyGwt implements EntryPoint {
 
 		// Create a text
 		final TextBox area = new TextBox();
-		area.setValue("2");
+		area.setValue("1");
 		RootPanel.get().add(area);
 
 		// Create a button
-		com.google.gwt.user.client.ui.Button b = new Button();
+		Button b = new Button();
 		b.setText("test call json restful service");
 
 		RootPanel.get().add(b);
+
+        CellTable<Ride> table = new CellTable<Ride>();
+
+        TextColumn<Ride> idColumn = new TextColumn<Ride>() {
+            @Override
+            public String getValue(Ride ride) {
+                return "" + ride.getId();
+            }
+        };
+        TextColumn<Ride> originColumn = new TextColumn<Ride>() {
+            @Override
+            public String getValue(Ride ride) {
+                return ride.getOrigin();
+            }
+        };
+        TextColumn<Ride> destColumn = new TextColumn<Ride>() {
+            @Override
+            public String getValue(Ride ride) {
+                return ride.getDestination();
+            }
+        };
+        TextColumn<Ride> leavingDateColumn = new TextColumn<Ride>() {
+            @Override
+            public String getValue(Ride ride) {
+                return ride.getLeavingDate().toString();
+            }
+        };
+        TextColumn<Ride> seatNumberColumn = new TextColumn<Ride>() {
+            @Override
+            public String getValue(Ride ride) {
+                return "" + ride.getSeatNumber();
+            }
+        };
+        TextColumn<Ride> driverColumn = new TextColumn<Ride>() {
+            @Override
+            public String getValue(Ride ride) {
+                return ride.getDriver().toString();
+            }
+        };
+        TextColumn<Ride> passengersColumn = new TextColumn<Ride>() {
+            @Override
+            public String getValue(Ride ride) {
+                return ride.getPassengers().toString();
+            }
+        };
+
+        table.addColumn(idColumn, "Id");
+        table.addColumn(originColumn, "Origin");
+        table.addColumn(destColumn, "Destination");
+        table.addColumn(leavingDateColumn, "Leaving Date");
+        table.addColumn(seatNumberColumn, "Seat Number");
+        table.addColumn(driverColumn, "Driver");
+        table.addColumn(passengersColumn, "Passengers");
+
+        final ListDataProvider<Ride> dataProvider = new ListDataProvider<Ride>();
+        dataProvider.addDataDisplay(table);
+
+
+        RootPanel.get().add(table);
 
 		b.addClickHandler(new ClickHandler() {
 
@@ -52,8 +117,8 @@ public class TaaJerseyGwt implements EntryPoint {
 						if (200 == response.getStatusCode()) {
 							IRide r = RideJsonConverter.getInstance()
 									.deserializeFromJson(response.getText());
-							Window.alert("get the ride from :" + r.getDriver());
-
+							Window.alert("get the ride from :" + r.getDestination());
+                            dataProvider.getList().add((Ride)r);
 						}
 					}
 				});
@@ -65,6 +130,5 @@ public class TaaJerseyGwt implements EntryPoint {
 
 			}
 		});
-
 	}
 }
