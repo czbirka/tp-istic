4956c254aee6e97c0f86f2a06f0556848c57f5d7
Thomas Daniellou <thomas.danll@gmail.com>
2014-10-23 12:16:37 +0200

    Add: Update controller

--- GLI/tpangular/app/scripts/app.js
+++ GLI/tpangular/app/scripts/app.js
@@ -9,28 +9,34 @@
  * Main module of the application.
  */
 var angularApp = angular.module('tpangularApp', [
-                    'ngCookies',
-                    'ngResource',
-                    'ngRoute',
-                    'ngSanitize',
-                    'ui.router'
-                  ]);
+	'ngCookies',
+	'ngResource',
+	'ngRoute',
+	'ngSanitize',
+	'ui.router'
+	]);
 
-angularApp.config(function ($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise('/home');
+ angularApp.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise('/home');
 
-    $stateProvider.state('home', {
-      url: '/home',
-      templateUrl: 'views/home.tpl.html',
-      controller: 'HomeCtrl'
-    })
-    .state('create', {
-      url: '/create',
-      templateUrl: 'views/create.tpl.html',
-      controller: 'CreateCtrl'
-   })
-   .state('delete', {
-      url: '/delete/:id',
-      controller: 'DeleteCtrl'
-   });
-});
+	$stateProvider.state('home', {
+		url: '/home',
+		templateUrl: 'views/home.tpl.html',
+		controller: 'HomeCtrl'
+	})
+	.state('create', {
+		url: '/create',
+		templateUrl: 'views/rideForm.tpl.html',
+		controller: 'CreateCtrl'
+	})
+	.state('update', {
+		url: '/update/:id',
+		templateUrl: 'views/rideForm.tpl.html',
+		controller: 'UpdateCtrl'
+	})
+	.state('delete', {
+		url: '/delete/:id',
+		controller: 'DeleteCtrl'
+	});
+}
+]);
--- GLI/tpangular/app/scripts/controllers/main.js
+++ GLI/tpangular/app/scripts/controllers/main.js
@@ -9,33 +9,58 @@
  */
 var controllers = angular.module('tpangularApp');
 
-controllers.controller('HomeCtrl', function ($scope, $http) {
-  	$http.get('/rest/rides').success(function (data) {
-  		$scope.rides = data;
-  	});
-});
+controllers.controller('HomeCtrl', ['$scope', '$http', 
+	function ($scope, $http) {
+		$http.get('/rest/rides').success(function (data) {
+			$scope.rides = data;
+		});
+	}
+]);
 
-controllers.controller('CreateCtrl', function ($scope, $state, $http) {
-    $scope.pageName = 'Create';
-    $scope.ride = {
-      origin: '',
-      destination: '',
-      leavingDate: '',
-      seatNumber: ''
-   };
+controllers.controller('CreateCtrl', ['$scope', '$state', '$http', 
+	function ($scope, $state, $http) {
+		$scope.pageName = 'Create';
+		$scope.ride = {
+			origin: '',
+			destination: '',
+			leavingDate: '',
+			seatNumber: ''
+		};
 
-   $scope.submit = function () {
-      $http.post('/rest/rides/create/', $scope.ride)
-      .success(function (){
-         $state.go('home');
-      });
-   };
-});
+		$scope.submit = function () {
+			$http.post('/rest/rides/create/', $scope.ride)
+			.success(function (){
+				$state.go('home');
+			});
+		};
+	}
+]);
 
+controllers.controller('UpdateCtrl', ['$state', '$stateParams', '$scope', '$http',
+	function ($state, $stateParams, $scope, $http) {
+		var id = $stateParams.id;
 
-controllers.controller('DeleteCtrl', function ($state, $stateParams, $scope, $http) {
-   $http.delete('/rest/rides/delete/' + $stateParams.id)
-   .success(function() {
-      $state.go('home');
-   });
-});
+		$scope.pageName = 'Update';
+
+		$http.get('/rest/rides/search/' + id)
+		.success(function (data) {
+			$scope.ride = data;
+		});
+
+		$scope.submit = function () {
+			$http.put('/rest/rides/update/' + id, $scope.ride)
+			.success(function () {
+				$state.go('home');
+			});
+		};
+	}
+]);
+
+controllers.controller('DeleteCtrl', ['$state', '$stateParams', '$scope', '$http', 
+	function ($state, $stateParams, $scope, $http) {
+		$http.delete('/rest/rides/delete/' + $stateParams.id)
+		.success(function() {
+			$state.go('home');
+		});
+	}
+]);
