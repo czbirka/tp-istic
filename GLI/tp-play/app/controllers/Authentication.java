f31cc68338f2501d3b8f074d85285e61096a1dd4
Amona Souliman <ammona.souliman1@gmail.com>
2014-11-12 10:28:34 +0100

    Add: Login & Logout implementation

--- GLI/tp-play/app/controllers/Authentication.java
+++ GLI/tp-play/app/controllers/Authentication.java
@@ -19,6 +19,7 @@ public class Authentication extends Controller {
      * Show the authentication form
      */
     public static Result login() {
+        //return ok(views.html.login.render(form(Login.class)));
         return ok(views.html.login.render(form(Login.class)));
     }
 
@@ -31,7 +32,18 @@ public class Authentication extends Controller {
      * Otherwise, the user must be authenticated (his user id should be stored into his session) and redirected to the index page.
      */
     public static Result authenticate() {
-        throw new NotImplementedError();
+        //throw new NotImplementedError();
+        Form<Login> loginForm = Form.form(Login.class).bindFromRequest();
+
+        if (loginForm.hasErrors()) {
+            return badRequest();
+        }
+        else {
+            session().clear();
+            session().put("username", loginForm.get().name);
+            return redirect(routes.Journeys.journeys());
+        }
+
         // TODO:
         // - Read the data of the form submission
         // - If data is valid, check that the user name and password are correct
@@ -43,7 +55,8 @@ public class Authentication extends Controller {
      * Logs out an user (remove his name from his session) and show a good bye message
      */
     public static Result logout() {
-        throw new NotImplementedError();
+        session().clear();
+        return ok(views.html.logout.render());
     }
 
     /**
@@ -70,7 +83,17 @@ public class Authentication extends Controller {
 
         // If needed, override this method to add a “global” validation rule (i.e not related to a particular field)
         public List<ValidationError> validate() {
-            return null;
+            List<ValidationError> errors = new ArrayList<ValidationError>();
+
+            if (name.equals("")) {
+                errors.add(new ValidationError("Username", "Invalid username"));
+            }
+
+            if (password.equals("")) {
+                errors.add(new ValidationError("Password", "Invalid password"));
+            }
+
+            return errors.isEmpty() ? null : errors;
         }
 
     }
