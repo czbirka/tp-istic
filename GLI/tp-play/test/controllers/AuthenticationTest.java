49e41a61fcc3be27c4ddc08b26a5b93bfa725104
Amona Souliman <ammona.souliman1@gmail.com>
2015-01-22 10:26:24 +0100

    Fix: separate test cases

--- GLI/tp-play/test/controllers/AuthenticationTest.java
+++ GLI/tp-play/test/controllers/AuthenticationTest.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import play.test.*;
 
@@ -7,44 +9,53 @@ import static org.fest.assertions.Assertions.assertThat;
 
 public class AuthenticationTest extends WithBrowser {
 
+    private final String username = "TARS";
+    private final String password = "tars";
+
     @Test
     public void signingUser() {
-
         // Try to go to the index page
         browser.goTo(routes.Journeys.rides().url());
         // User is redirected to the login page
         assertThat(browser.url()).isEqualTo(routes.Authentication.login().url());
 
         // Fill the signing in form
-        browser.fill("[name=username]").with("admin");
-        browser.fill("[name=password]").with("admin");
+        browser.fill("[name=username]").with(username);
+        browser.fill("[name=password]").with(password);
         browser.submit("form");
-        // User is logged in and redirected to the index page
-        // TODO
 
+        // User is logged in and redirected to the index page
         assertThat(browser.url()).isEqualTo(routes.Journeys.rides().url());
-            assertThat(browser.$("#currentUser", 0).getText()).isEqualTo("admin");
+        assertThat(browser.$("#currentUser", 0).getText()).isEqualTo(username);
 
         // Logout
-        // TODO
         browser.goTo(routes.Authentication.logout().url());
         assertThat(browser.url()).isEqualTo(routes.Authentication.login().url());
     }
 
     @Test
-    public void invalidLogin() {
+    public void emptyLogin() {
         browser.goTo(routes.Journeys.rides().url());
+
         // Submit an empty form
-        // TODO
         browser.submit("form");
+        assertThat(browser.url()).isEqualTo(routes.Authentication.login().url());
 
         // Validation error
         assertThat(browser.pageSource()).contains("has-error");
-
+    }
+    
+    @Test
+    public void invalidLogin() {
+        browser.goTo(routes.Journeys.rides().url());
+        
         // Submit an invalid form
-        browser.fill("[name=username]").with("admin");
-        browser.fill("[name=password]").with("1234");
+        browser.fill("[name=username]").with(username);
+        browser.fill("[name=password]").with("ABC");
         browser.submit("form");
+        assertThat(browser.url()).isEqualTo(routes.Authentication.login().url());
+
+        // Validation error
         assertThat(browser.pageSource()).contains("has-error");
     }
 }
